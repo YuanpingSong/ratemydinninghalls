22ea5c72e3f25049abf4b2c394993062b66705c5
Yuanping Song <yuanping.song@outlook.com>
2018-11-17 14:57:29 -0800

    drop down

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const dinningHalls = ['Bruin Plate', 'Covel', 'De Neve', 'Feast'];
 const dinningRestaurants = ['Bruin Cafe', 'Cafe 1919', 'Rondezvous', 'De Neve Grab and Go', 'The Study at Hedrick'];
 
 const flex_container = document.querySelector('.flex-container');
-const context_button = document.querySelector('button');
 const num_phantoms = 10; // set to maximum number of flex items we expect on a row - 1
 
 const context_halls = 0;
 const context_restaurants = 1;
 const context_all = 2;
-let flex_container_context = context_halls;
+let flex_container_context = context_halls; // set initial content here
 
 function init_flex_container(context) {
     if (context == context_all || context == context_halls) {
@@ -41,7 +40,21 @@ function flex_container_context_switch(new_context) {
     init_flex_container(new_context);
 }
 
-function context_button_on_click() {
+function halls_button_on_click() {
+    flex_container_context = context_halls;
+    flex_container_context_switch(flex_container_context);
+}
+function restaurants_button_on_click() {
+    flex_container_context = context_restaurants;
+    flex_container_context_switch(flex_container_context);
+}
+function all_button_on_click() {
+    flex_container_context = context_all;
+    flex_container_context_switch(flex_container_context);
+}
+
+
+/*function context_button_on_click() {
     switch (flex_container_context) {
         case context_halls:
             context_button.innerHTML = 'Restaurants';
@@ -55,9 +68,8 @@ function context_button_on_click() {
     }
     flex_container_context = (flex_container_context + 1) % 3;
     flex_container_context_switch(flex_container_context);
-}
+}*/
 
-context_button.addEventListener('click', context_button_on_click);
 init_flex_container(flex_container_context);
 
 
